c93bd1091833a567d4c3897c3d09166c20c6637a
Wilhans Souza <wilhans1984@hotmail.com>
2018-10-04 12:11:39 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email{ get; set; }
+        public DateTime BirtyDate { get; set; }
+        public double Basesalary { get; set; }
+        public Departament departament { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() {
+        }
+
+        public Seller(int id, string name, string email, DateTime birtyDate, double basesalary, Departament departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirtyDate = birtyDate;
+            Basesalary = basesalary;
+            this.departament = departament;
+        }
+
+        public void AddSales(SalesRecord sr) {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr) {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) {
+
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
